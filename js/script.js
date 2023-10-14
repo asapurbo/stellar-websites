71dd6b5e79df8b35436fab3ea5f783448a525d00
asapurbo <cstapurbo@gmail.com>
2023-10-15 00:40:49 +0600

    web1

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(function () {
+  $("#down_").click(function () {
+    $(".down").slideToggle();
+  });
+  $("#down_1").click(function () {
+    $(".down1").slideToggle();
+  });
+  $("#down_2").click(function () {
+    $(".down2").slideToggle();
+  });
+  $("#down_3").click(function () {
+    $(".down3").slideToggle();
+  });
+  $("#down_4").click(function () {
+    $(".down4").slideToggle();
+  });
+});
